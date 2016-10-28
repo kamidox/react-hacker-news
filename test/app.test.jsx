e9ab2e362e47df85f403b176ec6c14e2167193c9
Joey Huang <kamidox@gmail.com>
2016-10-28 16:38:41 +0800

    test: add more test and make test run on windows

--- test/app.test.jsx
+++ test/app.test.jsx
@@ -1,17 +1,29 @@
+/* eslint import/no-extraneous-dependencies: 0 */
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 
 import App from '../app/components/App';
+import NavLink from '../app/components/NavLink';
 
 describe('<App /> ', () => {
-  it('render a header', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find('.header')).to.have.length(1);
+  it('render header', () => {
+    const app = shallow(<App />);
+    expect(app.find('.header')).to.have.length(1);
   });
 
-  it('render a footer', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find('.footer')).to.have.length(1);
+  it('render content container', () => {
+    const app = shallow(<App />);
+    expect(app.find('.content')).to.have.length(1);
+  });
+
+  it('render 6 NavLinks', () => {
+    const app = shallow(<App />);
+    expect(app.find(NavLink)).to.have.length(6);
+  });
+
+  it('render footer', () => {
+    const app = shallow(<App />);
+    expect(app.find('.footer')).to.have.length(1);
   });
 });
